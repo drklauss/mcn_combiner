831ad62344deb912514bc367ef303cf12138180a
alog <dr.klauss@gmail.com>
2018-02-18 22:22:11 +0300

    read xlsx files into slices

--- price_handler.py
+++ price_handler.py
@@ -1,27 +1,17 @@
 import openpyxl
+import datetime
 import logging
 import os
 
 
-# shortNumberBook = openpyxl.load_workbook(filename='short_numbers.xlsx')
-# sheet = shortNumberBook.active
-
-
-# logging.info("ffffffffffff")
-# logging.critical("erwerwerer")
-
-
-# for i in sheet.rows:
-#     logging.info(i[1].value)
-#     print(i)
-
-
 class Combiner:
     STATISTICS_PRICE_SEARCH_PHRASE = 'statistics'
+    STATISTICS_COLUMNS_COUNT = 7
     CALLS_PRICE_SEARCH_PHRASE = 'calls'
+    CALLS_COLUMNS_COUNT = 8
     _statistics_price_name = _calls_price_name = None
-    _calls = []
-    _statistics = []
+    _statistics = []  # Собеседник, Время разговора, Время звонка, Стоимость c НДС
+    _calls = []  # Абонент, Собеседник, Время звонка
     _result_sheet = []
 
     def init_files(self) -> None:
@@ -46,18 +36,53 @@ class Combiner:
         logging.basicConfig(filename='price_combiner.log', format=log_format, level=logging.DEBUG)
 
     def read_statistic(self):
-        pass
+        """Вычитывает файл статистики"""
+        statistics_sheet = openpyxl.load_workbook(filename=self._statistics_price_name).active
+        rows_total = 0
+        unread_rows = []
+        logging.info('Start reading statistics file')
+        for i in statistics_sheet.rows:
+            rows_total += 1
+            row = tuple(filter(lambda x: x is not None, map(lambda x: x.value, i)))
+            if len(row) == self.STATISTICS_COLUMNS_COUNT:
+                try:
+                    call_time = datetime.datetime.strptime(row[0], '%d-%m-%Y %H:%M:%S').timestamp()
+                    call_duration = datetime.datetime.strptime(row[4], '%H:%M:%S').time()
+                    amount_with_nds = float(str.replace(row[6], ',', '.', -1)) * 1.18
+                    self._statistics.append(tuple((int(row[3]), call_duration, int(call_time), amount_with_nds)))
+                except ValueError:
+                    unread_rows.append(row)
+                    continue
+        unread_rows_count = rows_total - len(unread_rows)
+        if unread_rows_count != rows_total:
+            logging.warning('Rows total: {}, rows read: {}'.format(rows_total, unread_rows_count))
+            logging.warning('Unread rows: {}'.format(unread_rows))
 
     def read_calls(self):
+        """Вычитывает файл звонков"""
         calls_sheet = openpyxl.load_workbook(filename=self._calls_price_name).active
+        rows_total = 0
+        unread_rows = []
+        logging.info('Start reading calls file')
         for i in calls_sheet.rows:
-            print(*i)
-            row = filter(lambda x: x is not None, map(lambda x: x.value, i))
-            self._calls.append(tuple(row))
+            rows_total += 1
+            row = tuple(filter(lambda x: x is not None, map(lambda x: x.value, i)))
+            if len(tuple(row)) == self.CALLS_COLUMNS_COUNT:
+                try:
+                    self._calls.append(tuple((int(row[1]), int(row[3]), int(row[6].timestamp()))))
+                except ValueError:
+                    unread_rows.append(row)
+                    continue
+        unread_rows_count = rows_total - len(unread_rows)
+        if unread_rows_count != rows_total:
+            logging.warning('Rows total: {}, rows read: {}'.format(rows_total, unread_rows_count))
+            logging.warning('Unread rows: {}'.format(unread_rows))
 
     def run(self):
         """Запуск """
         self.init_logger()
         self.init_files()
+        self.read_statistic()
         self.read_calls()
+        print(self._statistics)
         print(self._calls)
