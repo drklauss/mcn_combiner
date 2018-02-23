e72134887eceaa3204679c51b861578676a05322
alog <dr.klauss@gmail.com>
2018-02-23 22:25:55 +0300

    xlsx combiner work!

--- price_handler.py
+++ price_handler.py
@@ -9,13 +9,31 @@ class Combiner:
     STATISTICS_COLUMNS_COUNT = 7
     CALLS_PRICE_SEARCH_PHRASE = 'calls'
     CALLS_COLUMNS_COUNT = 8
+    RESULT_FILE_NAME = 'result.xlsx'
     _statistics_price_name = _calls_price_name = None
-    _statistics = []  # Собеседник, Время разговора, Время звонка, Стоимость c НДС
-    _calls = []  # Абонент, Собеседник, Время звонка
+    _statistics = []  # Собеседник, Длительность разговора, Время звонка, Стоимость c НДС
+    _calls = []  # Абонент, Собеседник,  Время звонка, Ожидание звонка
     _result_sheet = []
+
+    @staticmethod
+    def init_logger() -> None:
+        """Инициализация логгера"""
+        log_format = u'%(asctime)s %(levelname)s %(filename)s:%(lineno)d %(message)s'
+        logging.basicConfig(filename='price_combiner.log', format=log_format, level=logging.DEBUG)
+
+    @staticmethod
+    def generate_row(stat_val: [], calls_val: []) -> tuple:
+        """Создает и возвращает строку"""
+        return calls_val[0], stat_val[0], stat_val[1], stat_val[3], stat_val[2]
+
+    @staticmethod
+    def log_rows_info(total: int, unread: []):
+        logging.warning('Rows total: {}, rows read: {}'.format(total, total - len(unread)))
+        logging.warning('Unread rows: {}'.format(unread))
 
     def init_files(self) -> None:
         """Поиск файлов прайсов"""
+        logging.info('Combiner is starting...')
         files = os.listdir('.')
         xlsx_files = tuple(filter(lambda x: '.xlsx' in x, files))
         if 2 > len(xlsx_files) or len(xlsx_files) > 2:
@@ -29,13 +47,7 @@ class Combiner:
             raise FileNotFoundError
         logging.info('Found prices: {}, {}'.format(self._statistics_price_name, self._calls_price_name))
 
-    @staticmethod
-    def init_logger() -> None:
-        """Инициализация логгера"""
-        log_format = u'%(asctime)s %(levelname)s %(filename)s:%(lineno)d %(message)s'
-        logging.basicConfig(filename='price_combiner.log', format=log_format, level=logging.DEBUG)
-
-    def read_statistic(self):
+    def _read_statistic(self):
         """Вычитывает файл статистики"""
         statistics_sheet = openpyxl.load_workbook(filename=self._statistics_price_name).active
         rows_total = 0
@@ -53,12 +65,11 @@ class Combiner:
                 except ValueError:
                     unread_rows.append(row)
                     continue
-        unread_rows_count = rows_total - len(unread_rows)
-        if unread_rows_count != rows_total:
-            logging.warning('Rows total: {}, rows read: {}'.format(rows_total, unread_rows_count))
-            logging.warning('Unread rows: {}'.format(unread_rows))
 
-    def read_calls(self):
+        if len(unread_rows) > 0:
+            self.log_rows_info(rows_total, unread_rows)
+
+    def _read_calls(self):
         """Вычитывает файл звонков"""
         calls_sheet = openpyxl.load_workbook(filename=self._calls_price_name).active
         rows_total = 0
@@ -69,20 +80,43 @@ class Combiner:
             row = tuple(filter(lambda x: x is not None, map(lambda x: x.value, i)))
             if len(tuple(row)) == self.CALLS_COLUMNS_COUNT:
                 try:
-                    self._calls.append(tuple((int(row[1]), int(row[3]), int(row[6].timestamp()))))
+                    self._calls.append(tuple((int(row[1]), int(row[3]), int(row[6].timestamp()), row[5].second)))
                 except ValueError:
                     unread_rows.append(row)
                     continue
-        unread_rows_count = rows_total - len(unread_rows)
-        if unread_rows_count != rows_total:
-            logging.warning('Rows total: {}, rows read: {}'.format(rows_total, unread_rows_count))
-            logging.warning('Unread rows: {}'.format(unread_rows))
+        if len(unread_rows) > 0:
+            self.log_rows_info(rows_total, unread_rows)
+
+    def _reports_comparator(self):
+        """Сравнивает отчеты и сохраняет результат в файл"""
+        wb = openpyxl.Workbook()
+        ws = wb.active
+        ws.title = 'result'
+        not_compare = []
+        for stat in self._statistics:
+            success = False
+            for call in self._calls:
+                row = self.generate_row(stat, call)
+                # Сверяем со временем ожидания + 2с, т.к. одна для окрулгения мкс, вторая для вхождения в этот интервал
+                if stat[0] == call[1] and stat[2] in range(call[2], call[2] + call[3] + 2):
+                    ws.append(row)
+                    success = True
+                    self._calls.remove(call)
+                    break
+            if not success:
+                not_compare.append(stat)
+        if len(not_compare) > 0:
+            err_sum = 0.0
+            for v in not_compare:
+                err_sum += v[3]
+            logging.warning("Not compared rows({} that costs {:.2f}): {}".format(len(not_compare), err_sum, not_compare))
+        logging.info("Lost calls ({}): {}".format(len(self._calls), self._calls))
+        wb.save(self.RESULT_FILE_NAME)
 
     def run(self):
         """Запуск """
         self.init_logger()
         self.init_files()
-        self.read_statistic()
-        self.read_calls()
-        print(self._statistics)
-        print(self._calls)
+        self._read_statistic()
+        self._read_calls()
+        self._reports_comparator()
